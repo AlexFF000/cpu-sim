db1ad6ea36954762c4be0b5c2d70a996d3f9b6ac
AlexFF000 <42391315+AlexFF000@users.noreply.github.com>
2018-08-26 19:33:15 +0100

    Began testing ALU functions

--- Code/Components/ALU/ALU.js
+++ Code/Components/ALU/ALU.js
@@ -10,34 +10,43 @@ function checkTypes(val1, val2){
 
 
 function splitNumbers(val1, val2){
+  console.log("Splitting number into list");
   // Split values into arrays of indvidual digits
   function backToNum(tmp){
     // Convert array of strings back to numbers
+    console.log("Turning strings to numbers");
+    console.dir(tmp);
     var arr = [];
     for(i=0; i < tmp.length; i++){
       arr.push(Number(tmp[0]));
       tmp.splice(0);
+      console.log("Strings are now numbers");
+      console.dir(arr);
       return arr;
 
     }
 
   }
   var tmp = val1.toString().split("");
+  console.log("Splitting strings");
   var numList1 = backToNum(tmp);
 
   tmp = val2.toString().split("");
   var numList2 = backToNum(tmp);
 
   var numList = [numList1, numList2];
+  console.dir(numList1);
   return numList;
 }
 
 function toNumber(arry){
   // Convert array back to number
+  console.log("Returning array to number")
   arry = arry.join();
   arry.replace(/'/g, "");
   arry.replace(/,/g, "");
   arry = Number(arry)
+  console.log("Returning number")
   return arry;
 }
 
@@ -129,6 +138,7 @@ function XOR(val1, val2){
     }
 
 function NOT(val1){
+  console.log("Starting NOT function");
   checkTypes(val1, 0);
   // Bitwise NOT
   var numList = splitNumbers(val1, 0);
@@ -142,5 +152,8 @@ function NOT(val1){
     }
     numList[0].splice(0)
   }
+  console.log("returning number");
+  console.log(outList);
   return toNumber(outList);
   }
+splitNumbers(0110, 1001);
